9d73cb8385119dc4d0ab1127f4fa624594541c84
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-02-16 01:41:04 +0530

    update the frontend live url

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.Frontent_Url ||"http://localhost:5000/api",
+  baseURL: process.env.Frontent_Url,
 });
 
 export default api;
