1386bc71adf285d8036c1196d9d3c8c1c78fef47
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-02-16 02:33:53 +0530

    fix the production issue

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//api
+//api..
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
 export default api;
