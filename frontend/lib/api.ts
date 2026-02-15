67e27280d303a8705618c6b5ece65eb5473a8bd5
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-02-16 03:30:06 +0530

    add baseurl in frontend

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 //api..
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
+  // baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: "https://csv-ai-backend.vercel.app/api",
 });
 
 export default api;
